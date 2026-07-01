47719e293637672454e33fd091192e14c79c6ad7
hoangducfu <duchvhe176767@fpt.edu.vn>
2026-07-01 07:35:19 +0700

    Initial commit

---
+++ src/controller/homeController.js
@@ -0,0 +1,10 @@
+const hanldeHelloWorld = (req, res) => {
+  return res.render("home.ejs");
+}
+const handleUserPage = (req, res) => {
+  return res.render("user.ejs");
+}
+module.exports = {
+  hanldeHelloWorld,
+  handleUserPage,
+};
---
+++ src/routes/web.js
@@ -0,0 +1,16 @@
+import express from "express";
+import  homeController  from "../controller/homeController";
+const router = express.Router();
+
+/**
+ * Init all web routes
+ * @param {*} app from express
+ */
+
+const initWebRoutes = (app) => {
+  router.get("/", homeController.hanldeHelloWorld);
+  router.get("/user", homeController.handleUserPage);
+  // webstite bắt đầu sau dấu /, ví dụ: localhost:8080/about thì sẽ vào router.get("/about")
+  app.use("/", router);
+}
+export default initWebRoutes;
